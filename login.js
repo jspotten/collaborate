a138a7dc47a47a6925dba9c8f7b83ad620edf74a
Jaden <jespotten@hotmail.com>
2023-03-04 18:02:44 -0700

    Change to Class to Enable ID Use for Other

--- login.js
+++ login.js
@@ -33,21 +33,38 @@ class Users extends User
     }
 }
 
-const existingUsers =
-[
-    {firstName: 'Billy', lastName: username: 'billyBob', email: 'billyBob@gmail.com', password: 'Bobbybill'},
-    {}
-];
-
-const user1 = new User('Billy', 'Bob', 'billyBob', )
-const users = new Users();
-
-function existingUser()
+class Task
 {
-    
+
 }
 
-function newUser()
+class TaskList
+{
+
+}
+
+const user1 = new User('Billy', 'Bob', 'billyBob', 'billyBob@gmail.com', 'Bobbybill');
+const user2 = new User('Jackie', 'Chan', 'jackieC', 'jackie.chan@hotmail.com', 'jackieR0cks!');
+const user3 = new User('Lizia', 'Stuart', 'lizia99', 'lizia.stuart@outlook.com', '\$myPassw0rd');
+const existingUsers = new Users(user1, user2, user3);
+
+function existingUserLogin()
+{
+    const loginKey = document.getElementById('#userOrEmail');
+    const password = document.getElementById('exUserPassword');
+    for(const user of existingUsers)
+    {
+        if(user.username === loginKey || user.email === loginKey)
+        {
+            if(user.password === password)
+            {
+                window.location.href = 'index.html';
+            }
+        }
+    }
+}
+
+function newUserCreation()
 {
 
 }
