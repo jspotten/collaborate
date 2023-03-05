a05824668e9c55c20c84471a4f8770688bc05026
Jaden <jespotten@hotmail.com>
2023-03-04 19:02:41 -0700

    Task and TaskList Class Definitions

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 class User
 {
-    constructor(firstName, lastName, email, username, password)
+    constructor(firstName, lastName, email, username, password, taskLists)
     {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.username = username;
         this.password = password;
+        this.taskLists = taskLists;
     }
 
     getJSON()
@@ -35,12 +36,58 @@ class Users extends User
 
 class Task
 {
+    constructor(title, date, pinned)
+    {
+        this.title = title;
+        this.date = date;
+        this.pinned = pinned;
+    }
 
+    pinList()
+    {
+        this.pinned = true;
+    }
+
+    unpinList()
+    {
+        this.pinned = false;
+    }
 }
 
 class TaskList
 {
+    constructor(pinned, ...tasks)
+    {
+        this.tasks = tasks;
+        this.pinned = pinned;
+    }
 
+    addTask(newTask)
+    {
+        this.tasks.push(newTask);
+    }
+
+    removeTask(title)
+    {
+        for(const [i,task] of tasks)
+        {
+            if(task.title == title)
+            {
+                this.tasks.splice(i, task)
+                break;
+            }
+        }
+    }
+
+    pinList()
+    {
+        this.pinned = true;
+    }
+
+    unpinList()
+    {
+        this.pinned = false;
+    }
 }
 
 export {User, Users, Task, TaskList};
