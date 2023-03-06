747792c9ea26af4d3f15b245b7ca656f2c51636c
Jaden <jespotten@hotmail.com>
2023-03-05 18:52:51 -0700

    Local Storage Capability

--- login.js
+++ login.js
@@ -3,17 +3,17 @@ export {existingUserLogin, newUserCreation};
 
 const taskList = new TaskList("CS 260 HW", false, new Task("Startup JS", new Date('March 10, 2023 11:59:59')))
 const user1 = new User('Billy', 'Bob', 'billyBob', 'billyBob@gmail.com', 'Bobbybill', [taskList]);
-const user2 = new User('Jackie', 'Chan', 'jackieC', 'jackie.chan@hotmail.com', 'jackieR0cks!', [taskList]);
-const user3 = new User('Lizia', 'Stuart', 'lizia99', 'lizia.stuart@outlook.com', '\$myPassw0rd', [taskList]);
-//const existingUsers = new Users(user1, user2, user3);
-localStorage.setItem("allUsers", new Users(user1, user2, user3));
-const existingUsers = localStorage.getItem("allUsers").entries();
+const user2 = new User('Jackie', 'Chan', 'jackieC', 'jackie.chan@hotmail.com', 'jackieR0cks!', [new TaskList("MSC", false)]);
+const user3 = new User('Lizia', 'Stuart', 'lizia99', 'lizia.stuart@outlook.com', '\$myPassw0rd', []);
+const existingUsers = new Users(user1, user2, user3);
+localStorage.setItem("allUsers", existingUsers);
 
 function existingUserLogin()
 {
     const loginKey = document.getElementById('userOrEmail').value;
     const password = document.getElementById('exUserPassword').value;
-    for(const user of existingUsers.users)
+    const usersOnFile = localStorage.getItem("allUsers");
+    for(const user of usersOnFile.users)
     {
         if(user.username === loginKey || user.email === loginKey)
         {
@@ -45,16 +45,17 @@ function newUserCreation()
         if(userVars === '')
         {
             complete = false;
-            break;
+            return;
         }
     }
 
     if(complete)
     {
-        existingUsers.addUser(newUser);
-        console.log(existingUsers);
+        const usersOnFile = localStorage.getItem("allUsers");
+        usersOnFile.addUser(newUser);
+        console.log(usersOnFile);
         localStorage.setItem("currentUser", newUser);
-        //window.location.href = 'index.html';
+        window.location.href = 'index.html';
     }
 }
 document.getElementById('signUpButton').addEventListener('click', newUserCreation);
