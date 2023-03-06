c954620d7349b2ca98d9d977a402938490b7e925
Jaden <jespotten@hotmail.com>
2023-03-05 18:45:25 -0700

    User Creation for Login page.

--- login.js
+++ login.js
@@ -5,7 +5,9 @@ const taskList = new TaskList("CS 260 HW", false, new Task("Startup JS", new Dat
 const user1 = new User('Billy', 'Bob', 'billyBob', 'billyBob@gmail.com', 'Bobbybill', [taskList]);
 const user2 = new User('Jackie', 'Chan', 'jackieC', 'jackie.chan@hotmail.com', 'jackieR0cks!', [taskList]);
 const user3 = new User('Lizia', 'Stuart', 'lizia99', 'lizia.stuart@outlook.com', '\$myPassw0rd', [taskList]);
-const existingUsers = new Users(user1, user2, user3);
+//const existingUsers = new Users(user1, user2, user3);
+localStorage.setItem("allUsers", new Users(user1, user2, user3));
+const existingUsers = localStorage.getItem("allUsers").entries();
 
 function existingUserLogin()
 {
@@ -28,6 +30,31 @@ document.getElementById('loginButton').addEventListener('click', existingUserLog
 
 function newUserCreation()
 {
+    const newUser  = new User
+    (
+        document.getElementById('firstName').value,
+        document.getElementById('lastName').value,
+        document.getElementById('email').value,
+        document.getElementById('username').value,
+        document.getElementById('password').value
+    );
+    
+    let complete = true;
+    for(const userVars in newUser)
+    {
+        if(userVars === '')
+        {
+            complete = false;
+            break;
+        }
+    }
 
+    if(complete)
+    {
+        existingUsers.addUser(newUser);
+        console.log(existingUsers);
+        localStorage.setItem("currentUser", newUser);
+        //window.location.href = 'index.html';
+    }
 }
-document.getElementById('signupButton').addEventListener('click', newUserCreation);
+document.getElementById('signUpButton').addEventListener('click', newUserCreation);
