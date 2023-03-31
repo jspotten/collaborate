0682105fbdf3ecd0fc1617283135699d542e494f
Jaden <jespotten@hotmail.com>
2023-03-30 18:30:57 -0600

    Third Party, Router, and Authtoken Genderation

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
-const { MongoClient } = require('mongodb');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
 
-const username = process.env.MONGOUSER;
-const password = process.env.MONGOPASSWORD;
-const hostname = process.env.MONGOHOSTNAME;
+const app = express();
+const database = require('./database');
+const { Proxy } = require('./proxy');
+const cookieName = 'authToken';
+const portNum = process.argv.length > 2 ? process.argv[2] : 4000;
 
-if (!username) 
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
+
+const apiRouter = express.Router;
+app.use(`/collaborate`, apiRouter);
+
+apiRouter.post(`/auth/generate`, async (req, resp) =>
 {
-    throw Error("Database not configured. Set environment variables");
-}
+    if(await (database.getUser(req.body.email) || database.getUser(req.body.username)))
+    {
+        resp.status(409).send({message: 'User Already Exists'});
+    }
+    else
+    {
+        const user = await database.createNewUser
+        (
+            req.body.firstName,
+            req.body.lastName,
+            req.body.email,
+            req.body.username,
+            req.body.password   
+        );
+        setUserCookie(resp, user.token);
+        resp.send({id: user.id,})
+    }
+});
+
